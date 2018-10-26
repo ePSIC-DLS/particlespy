222979faf6ef06577a3e16f84cbec68a2e422cf4
Tom Slater <TomSlater@users.noreply.github.com>
2018-10-26 14:11:40 +0100

    Added files for PyPI upload
    Added files necessary for PyPI upload for future packaging.

---
+++ ParticleSpy/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ParticleSpy",
+    version="0.0.1",
+    author="Thomas Slater",
+    author_email="tjaslater@gmail.com",
+    description="A package to perform particle segmentation and analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ePSIC-DLS/ParticleSpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
