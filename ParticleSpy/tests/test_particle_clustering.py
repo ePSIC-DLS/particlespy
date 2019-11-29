93604705a2b868c03115f8447b5da60a6b8ac8b4
Tom Slater <TomSlater@users.noreply.github.com>
2019-11-29 10:34:58 +0000

    Create test_particle_clustering.py

---
+++ ParticleSpy/tests/test_particle_clustering.py
@@ -0,0 +1,17 @@
+from ParticleSpy import api as ps
+import hyperspy.api as hs
+import os
+
+def test_clustering():
+    my_path = os.path.dirname(__file__)
+    
+    data = hs.load(os.path.join(os.par_dir(my_path), 'Data/SiO2 HAADF Image.hspy'))
+    
+    params = ps.parameters()
+    params.generate(threshold='otsu',watershed=True,min_size=5,rb_kernel=100)
+    
+    particles = ps.ParticleAnalysis(data,params)
+    
+    new_plists = particles.cluster_particles(properties=['area','circularity'])
+    
+    assert len(new_plists[0].list) == 43 or len(new_plists[0].list) == 59
