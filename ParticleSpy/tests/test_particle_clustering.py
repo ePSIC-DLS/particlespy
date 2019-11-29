b4f0b3ac01661e6bc36152c8f03ee3d27dba2f7f
Tom Slater <TomSlater@users.noreply.github.com>
2019-11-29 11:27:57 +0000

    Update test_particle_clustering.py

--- ParticleSpy/tests/test_particle_clustering.py
+++ ParticleSpy/tests/test_particle_clustering.py
@@ -1,11 +1,10 @@
 from ParticleSpy import api as ps
 import hyperspy.api as hs
-import os
+from pathlib import Path
 
 def test_clustering():
-    my_path = os.path.dirname(__file__)
     
-    data = hs.load(os.path.join(os.path.par_dir(my_path), 'Data/SiO2 HAADF Image.hspy'))
+    data = hs.load(str(Path(__file__).parent.parent / 'Data/SiO2 HAADF Image.hspy'))
     
     params = ps.parameters()
     params.generate(threshold='otsu',watershed=True,min_size=5,rb_kernel=100)
