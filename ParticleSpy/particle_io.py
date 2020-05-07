2ff58eb69f863568b6be39788f443cb8d49970d7
Mohsen Danaie <mohsen.danaie@diamond.ac.uk>
2020-05-07 21:38:45 +0100

    Updated save_plist and load_plist methods

--- ParticleSpy/particle_io.py
+++ ParticleSpy/particle_io.py
@@ -16,9 +16,26 @@ def save_plist(p_list,filename):
     for i, particle in enumerate(p_list.list):
         p_group = f.create_group("Particle "+str(i))
         
-        p_group.attrs["Area"] = particle.area
-        p_group.attrs["Area units"] = particle.area_units
-        p_group.attrs["Circularity"] = particle.circularity
+	p_group.attrs["Area"] = particle.properties['area']['value']
+        p_group.attrs["Area units"] = particle.properties['area']['units']
+        p_group.attrs["Equivalent circular diameter"] = particle.properties['equivalent circular diameter']['value']
+        p_group.attrs["Equivalent circular diameter units"] = particle.properties['equivalent circular diameter']['units']
+        p_group.attrs["X"] = particle.properties['x']['value']
+        p_group.attrs["X units"] = particle.properties['x']['units']
+        p_group.attrs["Y"] = particle.properties['y']['value']
+        p_group.attrs["Y units"] = particle.properties['y']['units']
+        p_group.attrs["Major axis length"] = particle.properties['major axis length']['value']
+        p_group.attrs["Major axis length units"] = particle.properties['major axis length']['units']
+        p_group.attrs["Minor axis length"] = particle.properties['minor axis length']['value']
+        p_group.attrs["Minor axis length units"] = particle.properties['minor axis length']['units']
+        p_group.attrs["Circularity"] = particle.properties['circularity']['value']
+        p_group.attrs["Eccentricity"] = particle.properties['eccentricity']['value']
+        p_group.attrs["Solidity"] = particle.properties['solidity']['value']
+        p_group.attrs["Intensity"] = particle.properties['intensity']['value']
+        p_group.attrs["Intensity_max"] = particle.properties['intensity_max']['value']
+        p_group.attrs["Intensity_std"] = particle.properties['intensity_std']['value']
+
+
         #p_group.attrs["Zone"] = particle.zone
         
         p_group.create_dataset("Mask",data=particle.mask)
@@ -39,6 +56,10 @@ def load_plist(filename):
             
             particle.set_area(p_group.attrs['Area'],p_group.attrs['Area units'])
             particle.set_circularity(p_group.attrs["Circularity"])
+            particle.set_circdiam(p_group.attrs["Equivalent circular diameter"],p_group.attrs["Equivalent circular diameter units"])
+            particle.set_axes_lengths([p_group.attrs["Major axis length"],p_group.attrs["Minor axis length"]],p_group.attrs["Major axis length units"])
+            particle.set_eccentricity(p_group.attrs["Eccentricity"])
+            particle.set_intensity(p_group.attrs["Intensity"])
             #particle.set_zone(p_group.attrs["Zone"])
             
             particle.set_mask(np.array(p_group['Mask'][:]))
