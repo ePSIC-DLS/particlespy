3061040f7d1f61b89cd4eeea2be904a5061153b7
Tom Slater <TomSlater@users.noreply.github.com>
2020-07-02 15:49:03 +0100

    Update test_plotting.py

--- ParticleSpy/tests/test_plotting.py
+++ ParticleSpy/tests/test_plotting.py
@@ -8,8 +8,8 @@ def test_plotting():
     
     p_list.plot('area')
     p_list.plot(['area','circularity'])
-    p_list.plot(['area','circularity','solidity'])
+    p_list.plot(['area','circularity','intensity'])
     
     ps.plot([p_list],'area')
     ps.plot([p_list],['area','circularity'])
-    ps.plot([p_list],['area','circularity','solidity'])
+    ps.plot([p_list],['area','circularity','intensity'])
