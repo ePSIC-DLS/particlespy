48310dbd1b06be12f858b340ace7b69e4f2ac03b
Tom Slater <TomSlater@users.noreply.github.com>
2020-07-02 15:43:57 +0100

    Add tests for 3D plotting

--- ParticleSpy/tests/test_plotting.py
+++ ParticleSpy/tests/test_plotting.py
@@ -8,6 +8,8 @@ def test_plotting():
     
     p_list.plot('area')
     p_list.plot(['area','circularity'])
+    p_list.plot(['area','circularity','solidity'])
     
     ps.plot([p_list],'area')
     ps.plot([p_list],['area','circularity'])
+    ps.plot([p_list],['area','circularity','solidity'])
