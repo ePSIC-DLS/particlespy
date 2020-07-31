e19dbd8aaeb1b96654a53e759df67a23474c85b1
YichiWang <yichi.wang.wyc@gmail.com>
2020-07-31 10:09:04 +0800

    Add reference for boundary following algorithm

--- ParticleSpy/boundary_analysis.py
+++ ParticleSpy/boundary_analysis.py
@@ -11,7 +11,11 @@ from scipy import optimize
 #   2D image boundary analysis
 
 def eight_neighbour_loop(center, start,boundary_matrix): 
-    """center is [x,y], start is [x,y] format"""
+    """
+    center is [x,y], start is [x,y] format
+    
+    ref: P796-798, Digital Image Processing (2008) Gonzalez & Woods
+    """
     x = center[0]
     y = center[1]
     neighbour_dic = {0: [x-1,y],
@@ -45,6 +49,7 @@ def eight_neighbour_loop(center, start,boundary_matrix):
 def boundary_follow(img, center=None):
     """
     Find boundaries and clock-wise follow boundary as a sequence list.
+    ref: P796-798, Digital Image Processing (2008) Gonzalez & Woods
     
     Parameters
     ----------
