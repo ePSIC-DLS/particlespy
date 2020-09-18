12865be41ae06965d03a1b8b7511d43356b3e80b
CameronGBell <cameronbell2236@gmail.com>
2020-09-18 16:01:11 +0100

    Assitional clustering tests
    added new clustering test capable of sets of parameters for each thresholding method

--- ParticleSpy/tests/test_particle_clustering.py
+++ ParticleSpy/tests/test_particle_clustering.py
@@ -14,3 +14,33 @@ def test_clustering():
     new_plists = particles.cluster_particles(properties=['area','circularity'])
     
     assert len(new_plists[0].list) == 45 or len(new_plists[0].list) == 57 or len(new_plists[0].list) == 43 or len(new_plists[0].list) == 59 or len(new_plists[0].list) == 99
+
+def test_clustering_all():
+    
+    data = hs.load(str(Path(__file__).parent.parent / 'Data/SiO2 HAADF Image.hspy'))
+    param_list = open(str(Path(__file__).parent.parent / 'Data/test_parameters.txt'), 'r')
+
+    for line in param_list:
+
+        line = line.strip("\n")
+        test_params, test_results = line.split(' ; ')
+        t_p = test_params.split(',')
+        test_results = test_results.split(',')
+        params = ps.parameters()
+        print(t_p[0])
+        params.generate(threshold=t_p[0], watershed=bool(int(t_p[1])), watershed_size=int(t_p[2]), watershed_erosion=int(t_p[3]), invert= bool(int(t_p[4])), min_size=int(t_p[5]), rb_kernel=int(t_p[6]), gaussian=int(t_p[7]), local_size=int(t_p[8]))
+        particles = ps.ParticleAnalysis(data,params)
+    
+        new_plists = particles.cluster_particles(properties=['area','circularity'])
+
+
+        verif = False
+        for value in test_results:
+            if len(new_plists[0].list) == int(value):
+                verif = True
+    
+        assert verif == True
+    
+    param_list.close()
+
+test_clustering_all()
