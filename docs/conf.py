1100430e8101e9c16ab6071137751303df37a90f
Tom Slater <TomSlater@users.noreply.github.com>
2020-10-26 09:12:50 +0000

    Update labels to v0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2020, ePSIC'
 author = 'ePSIC'
 
 # The short X.Y version
-version = '0.4.1'
+version = '0.5.0'
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
