3aff2194bd9c73c4b145a454c637cf642bc99aa8
Tom Slater <TomSlater@users.noreply.github.com>
2023-05-02 10:32:55 +0100

    Update conf.py to 0.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2021, ePSIC'
 author = 'ePSIC'
 
 # The short X.Y version
-version = '0.6.0'
+version = '0.6.2'
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '0.6.2'
 
 
 # -- General configuration ---------------------------------------------------
